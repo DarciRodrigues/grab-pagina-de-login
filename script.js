a4ac5f905934eaebb86a924c243e993efc1f93d0
Darci_Rodriigues <dro29558@gmail.com>
2021-11-07 14:32:00 -0300

    6 atualizaçao

---
+++ script.js
@@ -0,0 +1,8 @@
+function validarEmail() {
+    let email = document.querySelector('#email').value
+    let erro = document.querySelector('#msgerro')
+    console.log(email)
+    if () {
+        erro.innerHTML = "email"
+    }
+}
