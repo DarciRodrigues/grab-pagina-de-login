7672fb0087bfc7dac841535b0310f2ae6bcf61a2
Darci_Rodriigues <dro29558@gmail.com>
2021-11-09 09:33:13 -0300

    atualizaçao quase final

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function validarEmail() {
     let email = document.querySelector('#email').value
-    let erro = document.querySelector('#msgerro')
-    console.log(email)
-    if () {
-        erro.innerHTML = "email"
+    let erro = document.querySelector('.error-validation')
+    let re = /\S+@\S+\.\S+/;
+    if (!re.test(email)) {
+        erro.innerHTML = "por favor,insira um email válido"
     }
 }
