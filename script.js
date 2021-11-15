0a3462d416da6d0c801bdc618f1f373416e6bf2b
Darci Rodrigues <73960815+DarciRodrigues@users.noreply.github.com>
2021-11-15 14:35:05 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function validarEmail() {
-    let email = document.querySelector('#email').value
-    let erro = document.querySelector('.error-validation')
-    let re = /\S+@\S+\.\S+/;
-    if (!re.test(email)) {
-        erro.innerText = "por favor,insira um email válido"
-    }
-}
